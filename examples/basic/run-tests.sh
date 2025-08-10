f57dc0ed05f6d4e21cb0fd726d42f834c91db6c6
Cyril Michel HANSEN <cyril.hansen@gmail.com>
2025-08-11 00:36:43 +0200

    Remove mandelbrot from BASIC test suite

--- examples/basic/run-tests.sh
+++ examples/basic/run-tests.sh
@@ -63,7 +63,7 @@ PY
                 fi
         }
 
-        for t in hello relop adder string strfuncs instr gosub funcproc graphics readhplot circle box sudoku array_oob_read array_oob_write dim_expr mandelbrot pi baseconv mir_demo datediff date rnd_noarg; do
+        for t in hello relop adder string strfuncs instr gosub funcproc graphics readhplot circle box sudoku array_oob_read array_oob_write dim_expr pi baseconv mir_demo datediff date rnd_noarg; do
                 echo "Running $t"
                 run_test "$t"
                 echo "$t OK"
